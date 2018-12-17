72c90ddabddb9eaf28105eb64544a74510715cc5
wmin0 <wmin0@hotmail.com>
2018-12-17 16:33:23 +0800

    add contract import

--- contracts/index.js
+++ contracts/index.js
@@ -1,9 +1,15 @@
 const IERC20 = require('./IERC20.json')
 const ERC20Detailed = require('./ERC20Detailed.json')
 const IERC721 = require('./IERC721.json')
+const IERC721Metadata = require('./IERC721Metadata.json')
+const IERC721Enumerable = require('./IERC721Enumerable.json')
+const ERC721Full = require('./ERC721Full.json')
 
 module.exports = {
   IERC20,
   ERC20Detailed,
   IERC721,
+  IERC721Metadata,
+  IERC721Enumerable,
+  ERC721Full,
 }
